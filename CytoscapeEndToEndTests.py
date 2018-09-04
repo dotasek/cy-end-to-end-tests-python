7f12ffe2f391ac189ca73dbc22ae82a8889b2814
Unknown <dotasek.dev@gmail.com>
2018-09-04 14:39:11 -0400

    Added tests 1

--- CytoscapeEndToEndTests.py
+++ CytoscapeEndToEndTests.py
@@ -1,22 +1,25 @@
 import unittest
 import CyTestSupport
+import os.path
 
 cyCaller = CyTestSupport.CyCaller()
 
 
 class CytoscapeEndToEndTests(unittest.TestCase):
 
-    def test_upper(self):
+    def test_version(self):
         result = cyCaller.get("/v1/version")
         self.assertEqual(result['cytoscapeVersion'], '3.7.0-SNAPSHOT')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_all_apps_started(self):
+        result = cyCaller.get("/v1")
+        self.assertTrue(result['allAppsStarted'])
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+    def test_galfiltered(self):
+        path = os.path.join("resources", "galFiltered.cys")
+        abspath = os.path.abspath(path)
+        result = cyCaller.get("/v1/session?file=" + abspath)
+        self.assertEqual(result['file'], abspath)
 
 
 if __name__ == '__main__':
